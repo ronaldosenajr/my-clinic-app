f3cae6f74d08ae91ff572ba85d8f912940695634
ronaldosenajr <ronaldosenajr@gmail.com>
2021-12-12 02:09:27 -0400

    Impl: Header com 5 buttons ainda nao funcionais

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
         <Switch>
           <ScheduleProvider>
             <Route exact path="/main" component={ Main } />
