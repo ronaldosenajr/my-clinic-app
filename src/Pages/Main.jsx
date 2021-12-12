946cc3890233e51af72f58173198bec85f3f454e
ronaldosenajr <ronaldosenajr@gmail.com>
2021-12-12 15:58:11 -0400

    Impl: Novo header e a pagina Main mostra o conteudo da secao selecionada

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,13 +1,27 @@
 import React, { useContext } from 'react';
 import scheduleContext from '../Context/Context';
-import Header from '../Components/Header';
+import TabPanel from '../Components/TabPanel';
+import NewHeader from '../Components/NewHeader';
 
 export default function Main() {
   const { user } = useContext(scheduleContext);
+  const { tabValue } = useContext(scheduleContext);
   return (
     <div>
-      <Header />
-      <h3>{user.email}</h3>
+      <NewHeader />
+      <TabPanel value={ tabValue } index={ 0 }>
+        Usuario
+        {user.email}
+      </TabPanel>
+      <TabPanel value={ tabValue } index={ 1 }>
+        Pacientes
+      </TabPanel>
+      <TabPanel value={ tabValue } index={ 2 }>
+        Frequência
+      </TabPanel>
+      <TabPanel value={ tabValue } index={ 3 }>
+        Agenda
+      </TabPanel>
     </div>
   );
 }
