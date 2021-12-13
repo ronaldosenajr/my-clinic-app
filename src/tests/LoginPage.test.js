55043cab4167f50295ccafc0b7092c8d4a70763e
ronaldosenajr <ronaldosenajr@gmail.com>
2021-12-13 17:32:26 -0400

    Testa se os demais elementos aparecem na tela

--- src/tests/LoginPage.test.js
+++ src/tests/LoginPage.test.js
@@ -9,4 +9,22 @@ describe('Testa a Tela de Login', () => {
     const clinicAppTitle = screen.getByRole('heading', { level: 1, name: 'ClinicApp' });
     expect(clinicAppTitle).toBeInTheDocument();
   });
+
+  it('Se é exibido um Input com o placeholder "Email"', () => {
+    renderWithRouter(<App />);
+    const emailInput = screen.getByRole('textbox');
+    expect(emailInput).toHaveAttribute('placeholder', 'Email');
+  });
+
+  it('Se é exibido um Input com o placeholder "Senha"', () => {
+    renderWithRouter(<App />);
+    const passwordInput = screen.getByPlaceholderText('Senha');
+    expect(passwordInput).toHaveAttribute('placeholder', 'Senha');
+  });
+
+  it('Se é exibido um Botão com o texto "Entrar"', () => {
+    renderWithRouter(<App />);
+    const passwordInput = screen.getByRole('button', { name: 'ENTRAR' });
+    expect(passwordInput).toBeInTheDocument();
+  });
 });
