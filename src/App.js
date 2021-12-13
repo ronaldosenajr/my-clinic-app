6a95c0aaf4bcdd15b4f812cf4cad640783922239
ronaldosenajr <ronaldosenajr@gmail.com>
2021-12-13 17:08:02 -0400

    Finalizado o css e iniciando os testes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Login from './Pages/Login';
 import Main from './Pages/Main';
 import './App.css';
@@ -8,15 +8,12 @@ import ScheduleProvider from './Context/Provider';
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-        <Switch>
-          <ScheduleProvider>
-            <Route exact path="/main" component={ Main } />
-            <Route exact path="/" component={ Login } />
-          </ScheduleProvider>
-        </Switch>
-      </BrowserRouter>
+      <Switch>
+        <ScheduleProvider>
+          <Route exact path="/main" component={ Main } />
+          <Route exact path="/" component={ Login } />
+        </ScheduleProvider>
+      </Switch>
     );
   }
 }
