e1bbea19efe900d731b29572a979637b9ae67f01
ronaldosenajr <ronaldosenajr@gmail.com>
2021-12-16 14:14:14 -0400

    criado o forms para adicionar um novo paciente

--- src/Pages/Pacientes.jsx
+++ src/Pages/Pacientes.jsx
@@ -2,11 +2,27 @@ import React, { useState } from 'react';
 
 export default function Pacientes() {
   const [showCreatePatient, setShowCreatePatient] = useState(false);
+  const [patientName, setPatientName] = useState('');
+  const [answerableName, setAnswerableName] = useState('');
+  const [answerableEmail, setAnswerableEmail] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [newPatient, setNewPatient] = useState('');
 
   const handleCreatePatientClick = () => {
     setShowCreatePatient(true);
   };
 
+  const saveNewPatient = () => {
+    const patient = { nome: patientName,
+      responsavel: answerableName,
+      email: answerableEmail,
+      cpf };
+    setNewPatient(patient);
+    setPatientName('');
+    setAnswerableEmail('');
+    setAnswerableName('');
+    setCpf('');
+  };
   return (
     <div>
       <section>
@@ -24,6 +40,8 @@ export default function Pacientes() {
                 type="text"
                 id="nome-paciente-input"
                 placeholder="Nome do Paciente"
+                value={ patientName }
+                onChange={ ({ target }) => setPatientName(target.value) }
               />
             </label>
             <label htmlFor="nome-responsavel-input">
@@ -31,6 +49,8 @@ export default function Pacientes() {
                 type="text"
                 id="nome-responsavel-input"
                 placeholder="Nome do Responsável"
+                value={ answerableName }
+                onChange={ ({ target }) => setAnswerableName(target.value) }
               />
             </label>
             <label htmlFor="email-responsavel-input">
@@ -38,6 +58,8 @@ export default function Pacientes() {
                 type="email"
                 id="email-responsavel-input"
                 placeholder="Email do Responsável"
+                value={ answerableEmail }
+                onChange={ ({ target }) => setAnswerableEmail(target.value) }
               />
             </label>
             <label htmlFor="cpf-responsavel-input">
@@ -45,9 +67,17 @@ export default function Pacientes() {
                 type="number"
                 id="cpf-responsavel-input"
                 placeholder="CPF do Responsável"
+                value={ cpf }
+                onChange={ ({ target }) => setCpf(target.value) }
               />
             </label>
-            <button type="button">Salvar</button>
+            <button type="button" onClick={ saveNewPatient }>Salvar</button>
+            <button
+              type="button"
+              onClick={ (_) => setShowCreatePatient(false) }
+            >
+              Fechar
+            </button>
           </form>
         )}
       </section>
